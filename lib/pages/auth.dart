d13b8d98c2b977a0a684c9af632f813f9f4862c7
Borderlander103 <mailbox@brianrichardsononline.com>
2018-11-03 08:02:05 -0700

    Format Login page

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -9,18 +9,23 @@ class AuthPage extends StatelessWidget {
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'To verify that you are indeed you, \nplease push \nthe button below.*\n',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
-              textAlign: TextAlign.center,
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'To verify that you are indeed you, \nplease push \nthe button below.*\n',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+                  textAlign: TextAlign.center,
+                ),
+                RaisedButton(
+                  color: Theme.of(context).primaryColorDark,
+                  child: Text('LOGIN'),
+                  onPressed: () {},
+                ),
+              ],
             ),
-            RaisedButton(
-              color: Theme.of(context).primaryColorDark,
-              child: Text('LOGIN'),
-              onPressed: () {},
-            ),
+            Text('test'),
           ],
         ),
       ),
