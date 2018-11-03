b54433ab04850b235fb653372f8ea309096dfe3a
Borderlander103 <mailbox@brianrichardsononline.com>
2018-11-03 09:02:42 -0700

    Add bottom text note to login
    Figured out how to bottom align and center text.

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -9,23 +9,27 @@ class AuthPage extends StatelessWidget {
       ),
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  'To verify that you are indeed you, \nplease push \nthe button below.*\n',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
-                  textAlign: TextAlign.center,
-                ),
-                RaisedButton(
-                  color: Theme.of(context).primaryColorDark,
-                  child: Text('LOGIN'),
-                  onPressed: () {},
-                ),
-              ],
+            Expanded(child: Column()),
+            Text(
+              'To verify that you are indeed you, \nplease push \nthe button below.*\n',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+              textAlign: TextAlign.center,
             ),
-            Text('test'),
+            RaisedButton(
+              color: Theme.of(context).primaryColorDark,
+              child: Text('LOGIN'),
+              onPressed: () {},
+            ),
+            Expanded(child: Container()),
+            Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Text(
+                '*False claims punishable by up to 2 years hard counseling.',
+                textAlign: TextAlign.center,
+              ),
+            ),
           ],
         ),
       ),
