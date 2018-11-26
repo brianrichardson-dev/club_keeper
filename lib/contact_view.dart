248595681e73eb9e73af9bfcd2504057bae96101
Borderlander103 <mailbox@brianrichardsononline.com>
2018-11-26 14:07:31 -0800

    Add ContactList class

--- lib/contact_view.dart
+++ lib/contact_view.dart
@@ -8,11 +8,26 @@ class _ContactListItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new ListItem(
-      leading: new CircleAvatar(
-        child: new Text(_contact.fullName[0])
-      ),
-      title: new Text(_contact.fullName),
-      subtitle: new Text(_contact.email)
-    );
+        leading: new CircleAvatar(child: new Text(_contact.fullName[0])),
+        title: new Text(_contact.fullName),
+        subtitle: new Text(_contact.email));
+  }
+}
+
+class ContactList extends StatelessWidget {
+  final List<Contact> _contacts;
+
+  ContactList(this._contacts);
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialList(
+        type: MaterialListType.twoLine,
+        padding: new EdgeInsets.symmetric(vertical: 8.0),
+        children: _buildContactList());
+  }
+
+  List<_ContactListItem> _buildContactList() {
+    return _contacts.map((contact) => new _ContactListItem(_contact)).toList();
   }
 }
